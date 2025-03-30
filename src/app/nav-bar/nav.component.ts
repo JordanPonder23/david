b50880e8f28ae39e649cbc4a2151006a3874d871
Jordan Ponder <JordanPonder@protonmail.ch>
2025-03-29 21:42:46 -0800

    only meaningful change is inside nav component css where the animation repeat parameter is changed from 5 to 1. this why it was flashing

--- src/app/nav-bar/nav.component.ts
+++ src/app/nav-bar/nav.component.ts
@@ -14,6 +14,7 @@ export class NavComponent implements AfterViewInit {
     title = 'david-app';
     previewService: PreviewService = inject(PreviewService);
     currentPrev: string = "home"
+    pointable: Number = 1
     constructor() {
 
     }
@@ -23,41 +24,75 @@ export class NavComponent implements AfterViewInit {
         const homeLi = document.getElementById('homeLI') as HTMLAnchorElement;
 
         homeLi.addEventListener('mouseover', () => {
-           // console.log(this.grabPreviewState())
-            prev.classList.add("fade-in");
-            this.previewService.changeMessage("home")
+            console.log("Home: "+this.grabPreviewState() + ", from in: " + this.pointable)
+            // && this.grabPreviewState() != "home"
+            if(!prev.classList.contains("fade-in")  ) {
+                this.previewService.changeMessage("home")
+                prev.classList.add("fade-in");
+                this.pointable = 1
+            }
 
         });
         homeLi.addEventListener('mouseout', () => {
-            prev.classList.remove("fade-in")
-            prev.style.opacity = '0';
+            console.log("Home out: "+this.grabPreviewState() + ", from in: " + this.pointable)
+        // if(this.pointable == 1) {
+            var fadeIn =  document.getElementsByClassName('fade-in') as HTMLCollection
+            for (var i = 0; i < fadeIn.length; i++) {
+                var el = fadeIn[i] as HTMLElement
+                el.classList.remove("fade-in")
+                el.style.opacity = '0';
+            }
+            this.pointable = 0
+       // }
+        
         });
 
         const aboutLi = document.getElementById('aboutLI') as HTMLAnchorElement;
 
-
         aboutLi.addEventListener('mouseover', () => {
-           // console.log(this.grabPreviewState())
-            prev.classList.add("fade-in");
+            console.log("about: "+this.grabPreviewState() + ", from in: " + this.pointable)
+            
+           if(!prev.classList.contains("fade-in")) {
             this.previewService.changeMessage("about")
+                prev.classList.add("fade-in");
+                
+            }
 
         });
         aboutLi.addEventListener('mouseout', () => {
-            prev.classList.remove("fade-in")
-            prev.style.opacity = '0';
+            console.log("about out: "+this.grabPreviewState() + ", from in: " + this.pointable)
+        //    if(this.pointable == 1) {
+            var fadeIn =  document.getElementsByClassName('fade-in') as HTMLCollection
+            for (var i = 0; i < fadeIn.length; i++) {
+                var el = fadeIn[i] as HTMLElement
+                el.classList.remove("fade-in")
+                el.style.opacity = '0';
+                }
+         //   }
         });
 
         const projectsLi = document.getElementById("projectsLI") as HTMLAnchorElement;
 
         projectsLi.addEventListener('mouseover', () => {
-         //   console.log(this.grabPreviewState())
-            prev.classList.add("fade-in");
-            this.previewService.changeMessage("projects")
+            console.log("Projects: "+this.grabPreviewState() + ", from in: " + this.pointable)
+        
+            if(!prev.classList.contains("fade-in")) {
+                this.previewService.changeMessage("projects")
+              prev.classList.add("fade-in");
+            
+            }
 
         });
         projectsLi.addEventListener('mouseout', () => {
-            prev.classList.remove("fade-in")
-            prev.style.opacity = '0';
+            console.log("Projects out: "+this.grabPreviewState() + ", from in: " + this.pointable)
+         //   if(this.pointable == 1) {
+            var fadeIn =  document.getElementsByClassName('fade-in') as HTMLCollection
+            for (var i = 0; i < fadeIn.length; i++) {
+                var el = fadeIn[i] as HTMLElement
+                el.classList.remove("fade-in")
+                el.style.opacity = '0';
+                }
+         //  }
         });
         /*
         const extrasLi = document.getElementById("extrasLI") as HTMLAnchorElement;
@@ -78,14 +113,24 @@ export class NavComponent implements AfterViewInit {
         const contactsLi = document.getElementById("contactLI") as HTMLAnchorElement;
 
         contactsLi.addEventListener('mouseover', () => {
-        //    console.log(this.grabPreviewState())
-            prev.classList.add("fade-in");
-            this.previewService.changeMessage("contact")
+            console.log("Contact: "+this.grabPreviewState() + ", from in: " + this.pointable)
+            
+            if(!prev.classList.contains("fade-in")) {
+                this.previewService.changeMessage("contact")
+                prev.classList.add("fade-in");
+            }
 
         });
         contactsLi.addEventListener('mouseout', () => {
-            prev.classList.remove("fade-in")
-            prev.style.opacity = '0';
+            console.log("Contact out: "+this.grabPreviewState() + ", from in: " + this.pointable)
+        //    if(this.pointable == 1) {
+            var fadeIn =  document.getElementsByClassName('fade-in') as HTMLCollection
+            for (var i = 0; i < fadeIn.length; i++) {
+                var el = fadeIn[i] as HTMLElement
+                el.classList.remove("fade-in")
+                el.style.opacity = '0';
+               }
+        //   }
         });
     }
 
